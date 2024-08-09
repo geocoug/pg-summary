da3e6d1c01fb30367b751eefab4538ad532982d1
Caleb Grant <geocoug@gmail.com>
2024-08-09 14:22:09 -0700

    update README
    Signed-off-by: Caleb Grant <geocoug@gmail.com>

--- scripts/cli-example.sh
+++ scripts/cli-example.sh
@@ -2,6 +2,6 @@
 
 osascript -e 'tell application "Microsoft Excel" to close (every workbook whose name is "test.xlsx") saving no' >> /dev/null \
 && uv pip install -e . \
-&& pg_summary -i -v localhost -u docker -d dev -s information_schema -t tables -o test.xlsx \
+&& pg_summary -i -v localhost -u docker -d dev -s public -t books -o test.xlsx \
 && uv pip uninstall . \
 && open test.xlsx
